132cd9c185f4649be44e8c05a5faf9d456ec3452
elvin_dzhavadov <eljav04@mail.ru>
2024-12-17 16:11:23 +0400

    added Lesson_57

---
+++ Lesson_57_HT/Lesson_55_HT/Controllers/ContactController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Lesson_55_HT.Models;
+using Lesson_55_HT.Repository;
+
+
+namespace Lesson_55_HT.Controllers
+{
+    public class ContactController : Controller
+    {
+        public IActionResult Index()
+        {
+            ContactRepository contactRepository = new();
+            return View(contactRepository.ContactsList);
+        }
+
+        public IActionResult Details(int id)
+        {
+            ContactRepository contactRepository = new();
+
+            Contact? currentContact = contactRepository.GetByID(id);
+
+            if (currentContact is null)
+            {
+                return RedirectToAction("Index", "Contact");
+            }
+
+            return View(currentContact);
+        }
+
+
+    }
+}
+
