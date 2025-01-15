ab6c6493991842743c68f7a5ea189ad8acf6844d
Elvin Cavadov <eljav04@Elvins-MacBook-Air.local>
2025-01-15 14:25:55 +0400

    Edit Lesson_61

---
+++ Lesson_61_HT/Lesson_60_HT/Repos/ProductRepo.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Lesson_60_HT.Scaf_Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Lesson_60_HT.Repos
+{
+	public class ProductRepo
+	{
+		public List<Product> ProductsList { get; set; }
+        public List<Category> CategoriesList { get; set; }
+
+
+
+        public ProductRepo()
+		{
+			using var db = new ScafDbContext();
+
+			// Lazy
+			//CategoriesList = db.Categories.ToList();
+
+			// Eager
+			ProductsList = db.Products.Include(p => p.Category).ToList();
+
+
+			// Explicit 
+			//ProductsList.ForEach(pr =>
+			//db.Entry(pr).Reference(p => p.Category).Load()
+			//);
+		}
+	}
+}
+
---
+++ Lesson_61_HT/Lesson_60_HT/Scaf_Models/Product.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lesson_60_HT.Scaf_Models;
+
+public partial class Product
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; } = null!;
+
+    public decimal Price { get; set; }
+
+    public int CategoryId { get; set; }
+
+    public virtual Category Category { get; set; } = null!;
+}
